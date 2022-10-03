266f522dd1a5ee4565124ec7722422a9cbb5d80b
SheshankM <108621304+SheshankM@users.noreply.github.com>
2022-10-03 21:15:19 +0530

    Added About Us First half

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/About.module.css";
 
 export default function Home() {
-  return <div>Tech Affairs Website</div>;
+ 
+ 
+
+  function missionDisplay() {
+    var display = document.getElementById('content')
+    var missionText = missionContent()
+    display.textContent = missionText.toString()
+  }
+  function visionDisplay(){
+    var display = document.getElementById('content')
+    var visionText = visionContent()
+    display.textContent = visionText.toString()
+  }
+  function missionContent(){
+    return "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt magni commodi unde saepe magnam delectus aliquam quis! Sunt, iusto odio. "
+  }
+  function visionContent() {
+    return "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptate esse, exercitationem praesentium vitae magni necessitatibus nemo eos aliquid sequi."  
+  }
+ 
+ 
+  return (
+    <div>
+    <div className={styles.heading}>ABOUT US</div>
+    <div className={styles.details}>
+          <button className={styles.mission} onClick={missionDisplay}>MISSION</button>
+          
+          <button className={styles.vision}  onClick={visionDisplay}>VISION</button>
+    </div>
+    <div className={styles.vl}> </div>
+    <div id="content" className={styles.content}>
+      <p>
+        
+      </p>
+    </div>
+    </div>
+  )
 }
