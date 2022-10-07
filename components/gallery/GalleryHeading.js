c27a68653843111dddec00a264e83265de63e46b
Harsha Vasireddy <82756431+Harsha662002@users.noreply.github.com>
2022-10-07 17:46:06 +0530

    Delete GalleryHeading.js

--- components/gallery/GalleryHeading.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./gallery.module.css";
-
-const GalleryHeading = () => {
-  return <h3 className={styles.mainhead}>Recent Events</h3>;
-};
-
-export default GalleryHeading;
