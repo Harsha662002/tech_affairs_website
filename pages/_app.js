8573b8090690209f5532eb7a2189d93bc3e38f55
Harsha Vasireddy <82756431+Harsha662002@users.noreply.github.com>
2022-10-31 12:42:24 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,10 @@ function MyApp({ Component, pageProps }) {
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
           crossOrigin="anonymous"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+        ></link>
       </Head>
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
