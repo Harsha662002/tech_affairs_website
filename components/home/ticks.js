cb774386e42e9e03e8f6b540267735e2963a2dcf
Harsha Vasireddy <82756431+Harsha662002@users.noreply.github.com>
2022-10-31 23:03:33 +0530

    Update ticks.js

--- components/home/ticks.js
+++ components/home/ticks.js
@@ -30,11 +30,14 @@ const ticks = () => {
               <img src={`/assests/images/check.png`} />
             </div>
             <div className={styles.sub_flex_item_right}>
-              <div className={styles.sub_flex_item_right_top}>EXHIBITIONS</div>
+              <div className={styles.sub_flex_item_right_top}>
+                TECHNICAL EVENTS
+              </div>
               <div className={styles.sub_flex_item_right_bottom}>
-                To promote and encourage the interest of the students in
-                developing working devices and physical models and to conduct
-                the Annual Model Exhibition.
+                To promote and encourage computer-based activities including
+                creative software, graphics-based models, games, and other
+                challenging applications, and to organize competitions in this
+                area.
               </div>
             </div>
           </div>
@@ -45,11 +48,14 @@ const ticks = () => {
               <img src={`/assests/images/check.png`} />
             </div>
             <div className={styles.sub_flex_item_right}>
-              <div className={styles.sub_flex_item_right_top}>EXHIBITIONS</div>
+              <div className={styles.sub_flex_item_right_top}>VASHIHST</div>
               <div className={styles.sub_flex_item_right_bottom}>
-                To promote and encourage the interest of the students in
-                developing working devices and physical models and to conduct
-                the Annual Model Exhibition.
+                To conduct the technical festival VASHISHT for students annually
+                at the institute level in collaboration with academic
+                departments and clubs. The technical festival is intended to
+                focus on physical and computer models, posters on scientific and
+                technical topics, and presentations based on creative work and
+                organize various competitions to promote talents.
               </div>
             </div>
           </div>
@@ -62,11 +68,12 @@ const ticks = () => {
               <img src={`/assests/images/check.png`} />
             </div>
             <div className={styles.sub_flex_item_right}>
-              <div className={styles.sub_flex_item_right_top}>EXHIBITIONS</div>
+              <div className={styles.sub_flex_item_right_top}>
+                FOSTER COMPETITIVE TEAMS
+              </div>
               <div className={styles.sub_flex_item_right_bottom}>
-                To promote and encourage the interest of the students in
-                developing working devices and physical models and to conduct
-                the Annual Model Exhibition.
+                To help the competitive team get their resources and required
+                approvals from the institute side for their growth.
               </div>
             </div>
           </div>
@@ -77,11 +84,11 @@ const ticks = () => {
               <img src={`/assests/images/check.png`} />
             </div>
             <div className={styles.sub_flex_item_right}>
-              <div className={styles.sub_flex_item_right_top}>EXHIBITIONS</div>
+              <div className={styles.sub_flex_item_right_top}>
+                TECHNICAL MAGAZINES
+              </div>
               <div className={styles.sub_flex_item_right_bottom}>
-                To promote and encourage the interest of the students in
-                developing working devices and physical models and to conduct
-                the Annual Model Exhibition.
+                To bring out the technical magazines edited by students.
               </div>
             </div>
           </div>
@@ -92,11 +99,106 @@ const ticks = () => {
               <img src={`/assests/images/check.png`} />
             </div>
             <div className={styles.sub_flex_item_right}>
-              <div className={styles.sub_flex_item_right_top}>EXHIBITIONS</div>
+              <div className={styles.sub_flex_item_right_top}>LECTURES</div>
               <div className={styles.sub_flex_item_right_bottom}>
-                To promote and encourage the interest of the students in
-                developing working devices and physical models and to conduct
-                the Annual Model Exhibition.
+                To arrange lectures by eminent speakers on emerging areas of
+                science and technology.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.flex_container}>
+        <div className={styles.flex_item_left}>
+          <div className={styles.sub_flex_container}>
+            <div className={styles.sub_flex_item_left}>
+              <img src={`/assests/images/check.png`} />
+            </div>
+            <div className={styles.sub_flex_item_right}>
+              <div className={styles.sub_flex_item_right_top}>SEMINARS</div>
+              <div className={styles.sub_flex_item_right_bottom}>
+                To arrange seminars & Symposia in emerging areas of Science &
+                Technology.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.flex_item_middle}>
+          <div className={styles.sub_flex_container}>
+            <div className={styles.sub_flex_item_left}>
+              <img src={`/assests/images/check.png`} />
+            </div>
+            <div className={styles.sub_flex_item_right}>
+              <div className={styles.sub_flex_item_right_top}>
+                STUDENT CHAPTERS
+              </div>
+              <div className={styles.sub_flex_item_right_bottom}>
+                To promote the process of opening student chapters of
+                professional bodies in the Institute.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.flex_item_right}>
+          <div className={styles.sub_flex_container}>
+            <div className={styles.sub_flex_item_left}>
+              <img src={`/assests/images/check.png`} />
+            </div>
+            <div className={styles.sub_flex_item_right}>
+              <div className={styles.sub_flex_item_right_top}>
+                STUDENT PARTICIPATION
+              </div>
+              <div className={styles.sub_flex_item_right_bottom}>
+                To encourage and manage the participation of IIITDM students in
+                reputed national-level competitions
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.flex_container}>
+        <div className={styles.flex_item_left}>
+          <div className={styles.sub_flex_container}>
+            <div className={styles.sub_flex_item_left}>
+              <img src={`/assests/images/check.png`} />
+            </div>
+            <div className={styles.sub_flex_item_right}>
+              <div className={styles.sub_flex_item_right_top}>
+                STUDENT ACTIVITY
+              </div>
+              <div className={styles.sub_flex_item_right_bottom}>
+                To support specialized student activities with technical
+                content, e.g. the Robotics club.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.flex_item_middle}>
+          <div className={styles.sub_flex_container}>
+            <div className={styles.sub_flex_item_left}>
+              <img src={`/assests/images/check.png`} />
+            </div>
+            <div className={styles.sub_flex_item_right}>
+              <div className={styles.sub_flex_item_right_top}>INDUCTION</div>
+              <div className={styles.sub_flex_item_right_bottom}>
+                To induct fresh UG and PG students into technical activities in
+                a friendly manner.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.flex_item_right}>
+          <div className={styles.sub_flex_container}>
+            <div className={styles.sub_flex_item_left}>
+              <img src={`/assests/images/check.png`} />
+            </div>
+            <div className={styles.sub_flex_item_right}>
+              <div className={styles.sub_flex_item_right_top}>
+                TECHNICAL CLUBS
+              </div>
+              <div className={styles.sub_flex_item_right_bottom}>
+                To identify, support, and monitor competitive teams and clubs in
+                the area of technical activities.
               </div>
             </div>
           </div>
