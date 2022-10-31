4ca737ec4bf8b44845f4b3ba4bf62267d3b1d134
Harsha Vasireddy <82756431+Harsha662002@users.noreply.github.com>
2022-10-31 12:33:10 +0530

    Update resourcemangementcommitte.js

--- components/about/resourcemangementcommitte.js
+++ components/about/resourcemangementcommitte.js
@@ -8,19 +8,15 @@ function hexa_rmc(props) {
     <>
       <ul className={style.honeycomb} lang="es">
         <HoneyCell details={props.details[0]} />
-        <li
-          className={`${style.honeycombcell},${style.honeycomb_placeholder}`}
-        ></li>
-      </ul>
-      <ul className={style.honeycomb} lang="es">
         <HoneyCell details={props.details[1]} />
-        <HoneyCell details={props.details[2]} />
         <li
           className={`${style.honeycombcell},${style.honeycomb_placeholder}`}
         ></li>
       </ul>
       <ul className={style.honeycomb} lang="es">
+        <HoneyCell details={props.details[2]} />
         <HoneyCell details={props.details[3]} />
+        <HoneyCell details={props.details[4]} />
         <li
           className={`${style.honeycombcell},${style.honeycomb_placeholder}`}
         ></li>
