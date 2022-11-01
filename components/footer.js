0fbb6cace8f1f0cd636aad558ada6d776c083ac5
Harsha Vasireddy <82756431+Harsha662002@users.noreply.github.com>
2022-11-01 22:07:38 +0530

    Update footer.js

---
+++ components/footer.js
@@ -0,0 +1,68 @@
+import React from "react";
+import styles from "./footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import "../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
+// import "../../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
+const Footer = () => {
+  return (
+    <div>
+      <div className={styles.footer}>
+        <div className={styles.container2}>
+          <div className={styles.head3}> Technical Affairs</div>
+          <div className={styles.footerButtons}>
+            <button className={styles.button1}>
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLScv0bzrXxQ7QTX3Ra2tJlhe-S5nE6AxxhFJoLeOhdMrMwwR7g/viewform">
+                Feedback
+              </a>
+            </button>
+            <button className={styles.button2}>
+              <a href="https://sites.google.com/iiitdm.ac.in/technicalaffairs/disclaimer">
+                Disclaimer
+              </a>
+            </button>
+            <button className={styles.button3}>
+              <a href="https://docs.google.com/document/d/e/2PACX-1vTBfE7eqSa_k6AmRPQY6UyZ8PA18S-9bdpURkBfcBwc2prwVDpx06P0vWPY9PlC_rFrMoWGZ9aPomu2/pub">
+                Annual Report
+              </a>
+            </button>
+          </div>
+          <div className={styles.footcontent}>
+            <Link href="/developers">
+              <ul>
+                <li>
+                  <a> Developers Team</a>
+                </li>
+              </ul>
+            </Link>
+          </div>
+        </div>
+        <div className={styles.socials}>
+          <div className={styles.instagram}>
+            <a href="https://www.instagram.com/iiitdm.technical/">
+              <FontAwesomeIcon icon={faInstagram} size="lg" />
+            </a>
+          </div>
+          <div className={styles.linkedIn}>
+            <a href="https://www.linkedin.com/company/technical-affairs-iiitdm/mycompany/">
+              <FontAwesomeIcon icon={faLinkedin} size="lg" />
+            </a>
+          </div>
+          <div className={styles.mail}>
+            <a href="mailto:technical.affairs@iiitdm.ac.in">
+              <FontAwesomeIcon icon={faEnvelope} size="lg" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <p className={styles.copyright}>
+        © Copyright 2022 IIITDM Kancheepuram Technical Affairs
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
