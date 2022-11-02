b8f296a77feef44669d491b254d9f1ac04f82c09
Harsha Vasireddy <82756431+Harsha662002@users.noreply.github.com>
2022-11-02 09:45:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import HomeVision from "../components/home/homevision";
 import Ticks from "../components/home/ticks";
 import Circles from "../components/home/circles";
 import Footer from "../components/footer";
+import Events from "../components/home/events";
+import Eventlist from "../components/home/eventlist";
 export default function Home() {
   return (
     <div>
@@ -12,9 +14,11 @@ export default function Home() {
         <title>Tech Affairs IIITDM</title>
         <link rel="shortcut icon" href="/assests/images/techaffairslogo.ico" />
       </Head>
+      <Events />
       <Navbar />
       <HomeIntro></HomeIntro>
       <Circles></Circles>
+      <Eventlist />
       <Ticks></Ticks>
       <HomeVision></HomeVision>
       <Footer></Footer>
